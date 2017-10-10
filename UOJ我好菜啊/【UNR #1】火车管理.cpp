f0854a312acd22dae01e7ab2427f78f4e5eaec88
梁孜祺 <1525590627@qq.com>
2017-10-10 22:19:10 +0800

    线段树,可持久化线段树

--- UOJ我好菜啊/【UNR #1】火车管理.cpp
+++ UOJ我好菜啊/【UNR #1】火车管理.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #define inf 1e9
 #define N 500005
-#define M 8000005
+#define M 40000005
 #define mod 1000000007
 #define For(i,x,y) for(int i=x;i<=y;i++)
 #define Rep(i,x,y) for(int i=x;i>=y;i--)
@@ -13,11 +13,34 @@ inline void writeln(ll x){if(x<0) putchar('-'),x=-x;write(x);putchar('\n');}
 int n,m,type,ans,ind,root[N],p;
 int val[M],ls[M],rs[M];
 struct tree{int sum,tag;}t[N<<2];
+inline int merge(int x,int y){
+	if(!x) return y;
+	if(!y) return x;
+	++ind;ls[ind]=x;rs[ind]=y;
+	val[ind]=val[x];
+	return ind;
+}
 inline void pushdown(int k,int l,int r){
 	int tag=t[k].tag;t[k].tag=0;
-	if(l==r||!tag) return;
+	if(!tag) return;
 	int mid=(l+r)>>1;
-	t[k<<1].sum=
+	t[k<<1].tag=merge(tag,t[k<<1].tag);
+	t[k<<1|1].tag=merge(tag,t[k<<1|1].tag);
+	t[k<<1].sum=val[tag]*(mid-l+1);
+	t[k<<1|1].sum=val[tag]*(r-mid);
+}
+inline void insert(int k,int l,int r,int x,int y,int c){
+	if(l==x&&r==y){
+		t[k].tag=merge(c,t[k].tag);
+		t[k].sum=(r-l+1)*val[c];
+		return;
+	}
+	pushdown(k,l,r);
+	int mid=(l+r)>>1;
+	if(y<=mid) insert(k<<1,l,mid,x,y,c);
+	else if(x>mid) insert(k<<1|1,mid+1,r,x,y,c);
+	else insert(k<<1,l,mid,x,mid,c),insert(k<<1|1,mid+1,r,mid+1,y,c);
+	t[k].sum=t[k<<1].sum+t[k<<1|1].sum;
 }
 inline int query(int k,int l,int r,int x,int y){
 	if(l==x&&r==y) return t[k].sum;
@@ -26,6 +49,29 @@ inline int query(int k,int l,int r,int x,int y){
 	if(y<=mid) return query(k<<1,l,mid,x,y);
 	else if(x>mid) return query(k<<1|1,mid+1,r,x,y);
 	else return query(k<<1,l,mid,x,mid)+query(k<<1|1,mid+1,r,mid+1,y);
+}
+inline int erase(int &x){
+	int y=x;x=++ind;
+	ls[x]=ls[y];rs[x]=rs[y];
+	val[x]=val[y]; 
+	if(!ls[x]){x=0;return 1;}
+	if(erase(ls[x])==1) x=rs[x];
+	else val[x]=val[ls[x]];
+	return 0;
+}
+inline void Del(int k,int l,int r,int x){
+	if(l==r){
+		if(t[k].tag){
+			erase(t[k].tag);
+			t[k].sum=val[t[k].tag];
+		}
+		return;
+	}
+	pushdown(k,l,r);
+	int mid=(l+r)>>1;
+	if(x<=mid) Del(k<<1,l,mid,x);
+	else Del(k<<1|1,mid+1,r,x);
+	t[k].sum=t[k<<1].sum+t[k<<1|1].sum;
 }
 int main(){
 	n=read();m=read();type=read();
@@ -39,13 +85,14 @@ int main(){
 			writeln(ans);
 		}else if(opt==2){
 			int x=read();
+			x=(x+ans*type)%n+1;
 			Del(1,1,n,x);
 		}else{
-			int x=read(),y=read(),val=read();
+			int x=read(),y=read(),v=read();
 			x=(x+ans*type)%n+1;y=(y+ans*type)%n+1;
 			if(x>y) swap(x,y);
-			++tot;val[tot]=x;
-			insert(1,1,n,x,y,tot);
+			++ind;val[ind]=v;
+			insert(1,1,n,x,y,ind);
 		}
 	}
 	return 0;
