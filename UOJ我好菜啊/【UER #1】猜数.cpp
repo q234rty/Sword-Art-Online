6932195fc98051e15068bc980049fcca5e260f20
梁孜祺 <1525590627@qq.com>
2017-10-13 13:11:42 +0800

    lbc NOIP AK

--- UOJ我好菜啊/【UER #1】猜数.cpp
+++ UOJ我好菜啊/【UER #1】猜数.cpp
@@ -6,16 +6,17 @@
 #define For(i,x,y) for(int i=x;i<=y;i++)
 #define Rep(i,x,y) for(int i=x;i>=y;i--)
 using namespace std;
-typedef long long ll;
-inline int read(){int x=0,f=0;char ch=getchar();for(;ch<'0'||ch>'9';ch=getchar()) if(ch=='-') f=1;for(;ch>='0'&&ch<='9';ch=getchar()) x=(x<<1)+(x<<3)+ch-48;return f?-x:x;}
+typedef unsigned long long ll;
+inline ll read(){ll x=0,f=0;char ch=getchar();for(;ch<'0'||ch>'9';ch=getchar()) if(ch=='-') f=1;for(;ch>='0'&&ch<='9';ch=getchar()) x=(x<<1)+(x<<3)+ch-48;return f?-x:x;}
 inline void write(ll x){if(x<10) putchar(x+48);else write(x/10),putchar(x%10+48);}
 inline void writeln(ll x){if(x<0) putchar('-'),x=-x;write(x);putchar('\n');}
-int n;
-pair <int,string> s[10005];
+ll g,l;
 int main(){
-	n=read();
-	For(i,1,n) cin>>s[i].second,s[i].first=s[i].second.size();
-	sort(s+1,s+1+n);
-	For(i,1,n) cout<<s[i].second<<endl;
-	return 0;
+	ll T=read();
+	while(T--){
+		g=read();l=read();
+		ll mx=g+l;
+		l=l/g;ll mn=(ll)sqrt(l)*g;
+		printf("%llu %llu\n",mn*2,mx);
+	}
 }
